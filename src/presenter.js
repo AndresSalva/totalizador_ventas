f2247e5347786e1c9ad2029c3405152bb8a8b9be
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-08-29 09:52:15 -0400

    Mostrar cantidad items

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,12 @@
-import sumar from "./sumador";
+import mostrarCantidad from "./totalizador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const cantidad = document.querySelector("#cantidad");
+const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const cantidad_items = Number.parseInt(cantidad.value);
+  div.innerHTML = "Cantidad de items: " + mostrarCantidad(cantidad_items) + "</p>";
 });
